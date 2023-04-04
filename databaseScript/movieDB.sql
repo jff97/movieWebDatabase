ca414964de85b9dfe4ce82d2c12131cdbe2b0780
Quinn-McClure <100171762+Quinn-McClure@users.noreply.github.com>
2023-04-04 11:06:43 -0500

    skeleton of attributes

--- databaseScript/movieDB.sql
+++ databaseScript/movieDB.sql
@@ -2,50 +2,77 @@ CREATE DATABASE movieDatabase;
 
 --Tables
 CREATE TABLE Movie (
-
+    MovieId
+    TicketsSold
+    Rating
+    Language
+    ReleaseDate
+    Title
+    Summary
+    Seconds
+    Budget
 );
 
 CREATE TABLE Director (
-
+    DirId
+    DirLastName
+    DirFirstName
+    DOB
 );
 
 CREATE TABLE Award (
-
+    AwardId
+    AwardTitle
+    AwardType
+    YearGiven
 );
 
 CREATE TABLE ProductionStudio (
-
+    StudioId
+    StudioName
 );
 
 CREATE TABLE Genre (
-
+    GenreId
+    GenreName
 );
 
 CREATE TABLE Actor (
-
+    ActorId
+    Nationality
+    Age
+    DOB
+    FirstName
+    LastName
 );
 
 --Relationships
 CREATE TABLE NominatedFor (
-
+    AwardId
+    MovieId
 );
 
 CREATE TABLE Wins (
-
+    AwardId
+    MovieId
 );
 
 CREATE TABLE Produces (
-
+    StudioId
+    MovieId
 );
 
 CREATE TABLE ActsIn (
-
+    ActorId
+    MovieId
 );
 
 CREATE TABLE Describes (
-
-);
+    GenreId
+    MovieId
+); 
 
 CREATE TABLE DirectedBy (
-
+    MovieId
+    DirId
 );
