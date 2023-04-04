ae3017f8f2d8a2a2698771ecf0c34e1416f8ed7a
Quinn-McClure <100171762+Quinn-McClure@users.noreply.github.com>
2023-04-04 11:02:02 -0500

    skeleton of script

---
+++ databaseScript/movieDB.sql
@@ -0,0 +1,51 @@
+CREATE DATABASE movieDatabase;
+
+--Tables
+CREATE TABLE Movie (
+
+);
+
+CREATE TABLE Director (
+
+);
+
+CREATE TABLE Award (
+
+);
+
+CREATE TABLE ProductionStudio (
+
+);
+
+CREATE TABLE Genre (
+
+);
+
+CREATE TABLE Actor (
+
+);
+
+--Relationships
+CREATE TABLE NominatedFor (
+
+);
+
+CREATE TABLE Wins (
+
+);
+
+CREATE TABLE Produces (
+
+);
+
+CREATE TABLE ActsIn (
+
+);
+
+CREATE TABLE Describes (
+
+);
+
+CREATE TABLE DirectedBy (
+
+);
