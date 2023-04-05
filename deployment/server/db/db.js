4ab9166dd7575c984aa9616cb0d8452e9c51347d
Quinn-McClure <100171762+Quinn-McClure@users.noreply.github.com>
2023-04-05 07:26:51 -0500

    moved all db things into backend db file

--- deployment/server/db/db.js
+++ deployment/server/db/db.js
@@ -3,7 +3,7 @@ const knex = require('knex');
 const db = knex({
     client: 'sqlite3',
     connection: {
-        filename: ''//add file path to the db here
+        filename: ''//add file path to the .db file here
     },
     useNullAsDefault: true
 });
