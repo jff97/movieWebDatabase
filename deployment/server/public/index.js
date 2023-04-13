3ff44631a7b71e2a0542b1084cd197f7b9d45e09
Quinn-McClure <100171762+Quinn-McClure@users.noreply.github.com>
2023-04-13 14:56:40 -0500

    have a front end, backend not doing too hot

--- deployment/server/public/index.js
+++ deployment/server/public/index.js
@@ -1,8 +1,59 @@
-//fetch('')
-$.ajax({
-    url:'/movies',
-    type: 'GET',
-    dataType: 'json',
-    data: response.body,
-    success: 'success'
+document.getElementById('submitButton').addEventListener('click', function() {
+    //get input
+    var sql = document.getElementById('sqlInput').value;
+
+    //create data object
+    var inputData = {
+        sql: sql
+    };
+
+    //send HTTP request to the backend
+    fetch('/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(inputData)
+    })
+    .then(response => response.json())
+    .then(responseData => {
+        //handle res data
+        console.log(responseData);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+
+    var output = document.getElementById('output');
+    output.innerHTML = responseData;
 });
+
+// function sendQuery() {
+//     //get input
+//     var sql = document.getElementById('sqlInput').value;
+
+//     //create data object
+//     var inputData = {
+//         sql: sql
+//     };
+
+//     //send HTTP request to the backend
+//     fetch('/', {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify(inputData)
+//     })
+//     .then(response => response.json())
+//     .then(responseData => {
+//         //handle res data
+//         console.log(responseData);
+//     })
+//     .catch(error => {
+//         console.error('Error:', error);
+//     });
+// }
+
+// var output = document.getElementById('output');
+// output.innerHTML = result;
--- deployment/server/routes/index.routes.js
+++ deployment/server/routes/index.routes.js
@@ -1,12 +1,31 @@
 var express = require('express');
 var router = express.Router();
+const bodyParser = require('body-parser');
+const db = require('../db/db.js');
 const { getAllMovies, test } = require('../controllers/index.controller.js');
 // const db = require('../db/db');
+
+router.use(bodyParser.json());
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.json('index', { title: 'Express' });
   console.log('hello');
+});
+
+router.post('/', async function(req, res) {
+  const { sql } = req.body;
+
+  //process the data
+  const query = db.raw(sql);
+
+  //prep response data
+  const responseData = {
+    result: query,
+    message: 'Processed successfully'
+  };
+
+  res.json(responseData);
 });
 
 router.get('/movies', getAllMovies);
