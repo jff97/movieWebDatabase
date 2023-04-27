2f9e884e1cc4a7e775090e4d8f5fbc28774518c7
Quinn-McClure <100171762+Quinn-McClure@users.noreply.github.com>
2023-04-27 13:11:46 -0500

    fixed DOB stuff

--- deployment/server/db/db.js
+++ deployment/server/db/db.js
@@ -3,7 +3,7 @@ const knex = require('knex');
 const db = knex({
     client: 'sqlite3',
     connection: {
-        filename: './db/movieV2.db'//add file path to the .db file here
+        filename: './db/movieV6.db'//add file path to the .db file here
     },
     useNullAsDefault: true
 });
